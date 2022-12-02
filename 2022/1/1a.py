aed70288d827477f226e5cea32f3944fe4e23729
Jeff Hentschel <hentschel.jeff@gmail.com>
2022-12-02 10:02:18 -0500

    Add 2022 Day 1

---
+++ 2022/1/1a.py
@@ -0,0 +1,19 @@
+from pprint import pprint
+
+filename = "1.txt"
+
+lines = []
+with open(filename) as f:
+    lines = f.readlines()
+
+totals = []
+total = 0
+for line in lines:
+    if line == "\n":
+        totals.append(total)
+        total = 0
+    else:
+        total += int(line[:-1])
+
+totals.sort()
+print(sum(totals[-3:]))
